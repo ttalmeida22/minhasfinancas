606c943f00b37c96f863c2a10fbeabc9a947d395
thiago <almeida.thiago@gmail.com>
2022-02-05 20:49:02 -0300

    financas

---
+++ minhasfinancas/src/test/java/br/com/almeidathiago/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.almeidathiago.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
